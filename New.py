4fc2ea846da03d640499ddecc61cdfd12d7f1fbc
lakshjain1 <lakshjain0498@gmail.com>
2025-05-20 13:33:13 +0530

    update

--- New.py
+++ New.py
@@ -5,14 +5,10 @@ app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
 def get_data():
-    try:
-        with open('data.json', 'r') as file:
+    with open('data.json', 'r') as file:
           data = json.load(file)
-        return jsonify(data)  # Send JSON response
-    except FileNotFoundError:
-        return jsonify({"error": "Data file not found."}), 404
-    except json.JSONDecodeError:
-        return jsonify({"error": "Error decoding JSON data."}), 500
+    return jsonify(data) 
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
